5ff5892568457b05c8dd6b00f17296ef26217a0f
lixinSun <lixin.sun@dzx.co.jp>
2022-05-31 14:21:32 +0900

    販売店とクーポン機能

---
+++ public_html/app/Customize/Security/Http/Authentication/EccubeAuthenticationSuccessHandler.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Customize\Security\Http\Authentication;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Eccube\Entity\Customer;
+
+class EccubeAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        try {
+            $response = parent::onAuthenticationSuccess($request, $token);
+
+            $user = $token->getUser();
+            if(is_object($user)){
+                if($user instanceof Customer) {
+                    $ChainStore = $user->getChainStore();
+                    if(is_object($ChainStore)){
+                        return new RedirectResponse("/mypage");
+                    }
+                }
+            }
+        } catch (RouteNotFoundException $e) {
+            throw new BadRequestHttpException($e->getMessage(), $e, $e->getCode());
+        }
+
+        if (preg_match('/^https?:\\\\/i', $response->getTargetUrl())) {
+            $response->setTargetUrl($request->getUriForPath('/'));
+        }
+
+        return $response;
+    }
+}
